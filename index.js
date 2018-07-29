b2f1f1a7d612099f800dbd32afe29b3ba4af5b24
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-29 14:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ function makeInt(n){
 }
 //I don't know why this test is failing. It works perfectly when I console.log it using repl.it.
 function preserveDecimal(n){
-  parseFloat(n)
-  return n
+  return parseFloat(n)
 }
 //console.log(preserveDecimal('2.222'));
 
